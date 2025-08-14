a15aaf9e718400a758f6f1f9f1c9986b0458b7e3
lukashartnett <135441677+lukashartnett@users.noreply.github.com>
2025-08-14 15:07:08 +0000

    adding buttons

---
+++ script.js
@@ -0,0 +1,10 @@
+function purchase(amount) {
+  document.getElementById('thanks-message').textContent = `Thanks for purchasing for ${amount}!`;
+  document.getElementById('home').style.display = 'none';
+  document.getElementById('thanks').style.display = 'block';
+}
+
+function returnHome() {
+  document.getElementById('thanks').style.display = 'none';
+  document.getElementById('home').style.display = 'block';
+}
