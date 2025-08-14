44010a16ebe2a74d87ba4335d78f2c032830e715
lukashartnett <135441677+lukashartnett@users.noreply.github.com>
2025-08-14 21:24:37 +0000

    adding id labels

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function purchase(amount) {
-  document.getElementById('thanks-message').textContent = `Thanks for purchasing for ${amount}!`;
+  document.getElementById('thanks-message').textContent =
+    `Thanks for purchasing for ${amount}!`;
   document.getElementById('home').style.display = 'none';
   document.getElementById('thanks').style.display = 'block';
 }
